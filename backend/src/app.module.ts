048b3e5e791d5feb0bb21cf2e89af96417f3051d
Mbonu Chinedum <cboy.chinedu@gmail.com>
2026-05-29 19:18:23 +0100

    added the logs feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HomeModule } from './home/home.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { RequestLoggerMiddleware } from './logs/request-logger.middleware';
 
 @Module({
   imports: [
@@ -14,4 +15,9 @@ import { DashboardModule } from './dashboard/dashboard.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // This applies the CSV logger to every route in your application
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
